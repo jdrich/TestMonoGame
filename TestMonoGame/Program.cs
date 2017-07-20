9f381aebf0c2f22d4605e9fd80a011bdb930a179
Jonathan <jdrich@gmail.com>
2017-07-19 23:35:32 -0400

    Initial commit.

---
+++ TestMonoGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestMonoGame
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new TestGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
