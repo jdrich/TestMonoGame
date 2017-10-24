b4f03b5d1646f4f09fb248bee9a63b0821b337be
Jonathan Rich <jdrich@gmail.com>
2017-10-24 16:19:58 -0400

    Juggling.

--- TestMonoGame/Debug.cs
+++ TestMonoGame/Debug.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TestMonoGame
 {
@@ -11,9 +12,14 @@ namespace TestMonoGame
 		protected int LastFrames = 0;
 		protected int Fps = 0;
 
-		public Debug ()
+        protected SpriteFont debugFont;
+        protected GraphicsDeviceManager graphics;
+
+		public Debug (SpriteFont debugFont, GraphicsDeviceManager graphics)
 		{
-		}
+            this.debugFont = debugFont;
+            this.graphics = graphics;
+        }
 
 		public void Update(GameTime gameTime) {
 			UpdateFps (gameTime);
@@ -32,14 +38,48 @@ namespace TestMonoGame
 			}
 		}
 
-		public void Draw() {
+		public void Draw(GraphicsDevice graphicsDevice, GameTime gameTime) {
 			if (Display) {
-				DrawFps ();
+				DrawFps(graphicsDevice, gameTime);
 			}
 		}
 
-		protected void DrawFps() {
-		}
-	}
+		protected void DrawFps(GraphicsDevice graphics, GameTime gameTime) {
+
+            var drawColor = Color.White;
+
+            var width = this.graphics.PreferredBackBufferWidth;
+            var height = this.graphics.PreferredBackBufferHeight;
+
+            SpriteBatch spriteBatch = new SpriteBatch(graphics);
+
+            spriteBatch.Begin();
+
+            var fpsString = "FPS: " + Fps;
+            var resolutionString = "Resolution: " + width.ToString() + "," + height.ToString();
+
+            var fpsWidthHeight = debugFont.MeasureString(fpsString);
+            var resWidthHeight = debugFont.MeasureString(resolutionString);
+
+            var offset = 20;
+
+            spriteBatch.DrawString(
+                debugFont, 
+                fpsString, 
+                new Vector2(width - offset - fpsWidthHeight.X, height - offset - fpsWidthHeight.Y),
+                drawColor
+            );
+
+            spriteBatch.DrawString(
+                debugFont, 
+                resolutionString, 
+                new Vector2(width - offset - resWidthHeight.X, height - offset - resWidthHeight.Y - fpsWidthHeight.Y), 
+                drawColor
+            );
+
+            spriteBatch.End();
+
+        }
+    }
 }
 
--- TestMonoGame/TestGame.cs
+++ TestMonoGame/TestGame.cs
@@ -1,22 +1,66 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Windows.Forms;
+
+using Keys = Microsoft.Xna.Framework.Input.Keys;
 
 namespace TestMonoGame
 {
     public class TestGame : Game
     {
+        protected bool UseFullscreen = false;
+
+        protected bool MouseOn = true;
+
+        protected Vector2 screenWidthHeight;
+        protected IntVector2 lastScreenWidthHeight;
+
 		protected Debug debug;
 
 		protected KeyUpListener keyUpListener;
 
         protected GraphicsDeviceManager graphics;
         
+        //Camera
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+
+        //BasicEffect for rendering
+        BasicEffect basicEffect;
+
+        //Geometric info
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
         public TestGame()
         {
             graphics = new GraphicsDeviceManager(this);
-                        
-            Content.RootDirectory = "Content";
+
+            screenWidthHeight = new Vector2(0, 0);
+            lastScreenWidthHeight = new IntVector2();
+
+            Content.RootDirectory = "Content/bin";
+        }
+
+        protected void ClientSizeChanged(object sender, EventArgs e)
+        {
+            int width = Window.ClientBounds.Width;
+            int height = Window.ClientBounds.Height;
+
+            if (screenWidthHeight.X != width || screenWidthHeight.Y != height)
+            {
+                screenWidthHeight.X = width;
+                screenWidthHeight.Y = height;
+                
+                graphics.PreferredBackBufferWidth = width;
+                graphics.PreferredBackBufferHeight = height;
+                graphics.ApplyChanges();
+            }
         }
 
 		protected void ToggleDebug() {
@@ -26,18 +70,85 @@ namespace TestMonoGame
         protected override void Initialize()
         {
             base.Initialize();
+            
+            Mouse.WindowHandle = Window.Handle;
 
-			keyUpListener = new KeyUpListener ();
+            keyUpListener = new KeyUpListener ();
 
 			SetupFullscreenToggleListener ();
 			SetupExitListener ();
+            SetupDebugToggleListener();
 
-			debug = new Debug ();
+            debug = new Debug (Content.Load<SpriteFont>("Debug"), graphics);
+
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += new EventHandler<EventArgs>(ClientSizeChanged);
+
+            Init3d();
+        }
+
+        protected void Init3d()
+        {
+            //Setup Camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                               MathHelper.ToRadians(45f),
+                               GraphicsDevice.DisplayMode.AspectRatio,
+                1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
+                          Forward, Vector3.Up);
+
+            //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1f;
+
+            // Want to see the colors of the vertices, this needs to be on
+            basicEffect.VertexColorEnabled = true;
+
+            //Lighting requires normal information which VertexPositionColor does not have
+            //If you want to use lighting and VPC you need to create a custom def
+            basicEffect.LightingEnabled = false;
+
+            //Geometry  - a simple triangle about the origin
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(
+                                  0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-
+                                  20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(
+                                  20, -20, 0), Color.Blue);
+
+            //Vert buffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(
+                           VertexPositionColor), 3, BufferUsage.
+                           WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices);
         }
 
 		protected void SetupFullscreenToggleListener() {
 			keyUpListener.Listen (Keys.F11, (key) => {
-				graphics.ToggleFullScreen();
+                UseFullscreen = !UseFullscreen;
+
+                if (UseFullscreen)
+                {
+                    lastScreenWidthHeight.X = graphics.PreferredBackBufferWidth;
+                    lastScreenWidthHeight.Y = graphics.PreferredBackBufferHeight;
+
+                    graphics.PreferredBackBufferWidth = ScreenBounds().X;
+                    graphics.PreferredBackBufferHeight = ScreenBounds().Y;
+
+                    graphics.IsFullScreen = true;
+                } else
+                {
+                    graphics.PreferredBackBufferWidth = lastScreenWidthHeight.X;
+                    graphics.PreferredBackBufferHeight = lastScreenWidthHeight.Y;
+
+                    graphics.IsFullScreen = false;
+                }
+
 				graphics.ApplyChanges();
 			});
 		}
@@ -60,18 +171,107 @@ namespace TestMonoGame
 
         protected override void Update(GameTime gameTime)
 		{
-            base.Update(gameTime);
-
 			keyUpListener.Update();
-			debug.Update (gameTime);
+            debug.Update(gameTime);
+
+            Update3d(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected void Update3d(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camPosition.X -= 1f;
+                camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camPosition.X += 1f;
+                camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camPosition.Y -= 1f;
+                camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camPosition.Y += 1f;
+                camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 1f;
+            }
+            
+            Matrix rotationMatrix = Matrix.CreateRotationY(
+                                    MathHelper.ToRadians(1f));
+            camPosition = Vector3.Transform(camPosition,
+                          rotationMatrix);
+
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         Vector3.Up);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.DarkSlateGray);
-            base.Draw(gameTime);
+            GraphicsDevice.Clear(Color.DarkGray);
 
-			debug.Draw();
+            Draw3d(gameTime);
+                        
+			debug.Draw(graphics.GraphicsDevice, gameTime);
+
+            DrawMouse();
+
+            base.Draw(gameTime);
         }
+
+        protected void DrawMouse()
+        {
+            var x = Mouse.GetState().X;
+            var y = Mouse.GetState().Y;
+        }
+
+        protected void Draw3d(GameTime gametime)
+        {
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+            
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Turn off culling so we see both sides of our rendered triangle
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.
+                    Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.
+                                              TriangleList, 0, 3);
+            }
+        }
+
+        protected IntVector2 ScreenBounds()
+        {
+            var screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            var screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            return new IntVector2() { X = screenWidth, Y = screenHeight };
+        }
+    }
+
+    public struct IntVector2
+    {
+        public int X;
+        public int Y;
     }
 }
